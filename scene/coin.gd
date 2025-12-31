83442477f98eacb13a9677ed6ae2012c748de297
ilycointypo_ <ilyanirohaimi@gmail.com>
2026-01-01 00:39:21 +0800

    coin collection n player HUD
    coin collection tinggal letak actual coin
    player HUD coin shje yg function

--- scene/coin.gd
+++ scene/coin.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var game_manager: Node = $"../../GameManager"
 
 
 var picked_up = false
@@ -11,7 +12,7 @@ func _on_body_entered(body: Node2D) -> void:
 	if picked_up == false:
 		
 		picked_up = true
-		print ("+1 coin")
+		game_manager.add_score()
 		animated_sprite_2d.visible = false
 		audio_stream_player_2d.play()
 		await get_tree().create_timer(0.5).timeout
