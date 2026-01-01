ffdb0889ab7e4cbed2ef2a9dd554de06b041e62f
ilycointypo_ <ilyanirohaimi@gmail.com>
2026-01-02 00:52:09 +0800

    setel errorrrr gaisekk

--- scripts/gamemanager.gd
+++ scripts/gamemanager.gd
@@ -1,22 +1,22 @@
 extends Node
-
-@onready var score: Control = $"../CanvasLayer/Control/VBoxContainer/score/Label"
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	
-	score.text = str(GameState.score)
-
-
-func update_player_health(amount: int):
-	GameState.player_health += amount
-	print(GameState.player_health)
-
-func add_score():
-	GameState.score += 1
-	score.text = str(GameState.score)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#
+#@onready var score: Control = $"../CanvasLayer/Control/VBoxContainer/score/Label"
+#
+## Called when the node enters the scene tree for the first time.
+#func _ready() -> void:
+	#
+	#score.text = str(GameState.score)
+#
+#
+#func update_player_health(amount: int):
+	#GameState.player_health += amount
+	#print(GameState.player_health)
+#
+#func add_score():
+	#GameState.score += 1
+	#score.text = str(GameState.score)
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
--- scripts/gamestate.gd
+++ scripts/gamestate.gd
@@ -1,12 +1,12 @@
 extends Node
-
-var score: int = 0
-var level: int = 1
-var player_health: int = 10
-
-
-func reset():
-	score = 0
-	level = 1
-	player_health = 10
-	
+#
+#var score: int = 0
+#var level: int = 1
+#var player_health: int = 10
+#
+#
+#func reset():
+	#score = 0
+	#level = 1
+	#player_health = 10
+	#
