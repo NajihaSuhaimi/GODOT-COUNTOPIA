16099be0907ffeae9b6b6ee4fe6be9128fb03228
ilycointypo_ <ilyanirohaimi@gmail.com>
2026-01-11 20:34:20 +0800

    dah letak bunyi button

--- scene/win_screen.gd
+++ scene/win_screen.gd
@@ -1,8 +1,11 @@
 extends Control
 
+@onready var bunyiwin: AudioStreamPlayer2D = $bunyiwin
+@onready var button_biasa: AudioStreamPlayer2D = $"button biasa"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	bunyiwin.play()
 	pass # Replace with function body.
 
 
@@ -12,8 +15,12 @@ func _process(delta: float) -> void:
 	
 
 func _on_main_menu_pressed() -> void:
+	button_biasa.play()
+	await button_biasa.finished
 	get_tree().change_scene_to_file("res://scene/main_menu.tscn")
 
 
 func _on_quit_pressed() -> void:
+	button_biasa.play()
+	await button_biasa.finished
 	get_tree().quit()
